e1a05ef8b2d924128017b5670bd0a0ed5ec33a84
Ikraam <IbnDaanis@Gmail.com>
2021-06-28 23:15:18 +0100

    Add todo query

--- server/server.js
+++ server/server.js
@@ -1,15 +1,20 @@
 import express from 'express'
 import cors from 'cors'
-import { pool } from './database/database'
+import { pool } from './database/database.js'
 
 const app = express()
 
-app.use(cors)
+app.use(cors())
 app.use(express.json())
 
 app.post('/todos', async (req, res) => {
   try {
-    res.json(req.body)
+    const { description } = req.body
+    const newTodo = (
+      await pool.query('INSERT INTO todo (description) VALUES ($1) RETURNING *', [description])
+    ).rows[0]
+
+    res.json(newTodo)
   } catch (error) {
     console.error(error.message)
   }
