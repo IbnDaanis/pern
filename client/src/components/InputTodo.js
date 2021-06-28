f0c49b4963d82a3c910a364cc653433427f778d2
Ikraam <IbnDaanis@Gmail.com>
2021-06-29 00:59:31 +0100

    InputTodo styling

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -2,9 +2,16 @@ import React, { useState } from 'react'
 
 export const InputTodo = () => {
   const [state, setState] = useState(0)
+
   return (
     <>
-      <h1>Input Todo</h1>
+      <h1 className='text-center my-5'>Input Todo</h1>
+      <form className='d-flex'>
+        <input type='text' className='form-control' placeholder='Add Todo' />
+        <button type='submit' className='btn btn-success '>
+          Add
+        </button>
+      </form>
     </>
   )
 }
