1000c42dd5ff18132db9766efa22c91b38c711df
Ikraam <IbnDaanis@Gmail.com>
2021-06-29 01:01:09 +0100

    input todo use state form control

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -1,13 +1,19 @@
 import React, { useState } from 'react'
 
 export const InputTodo = () => {
-  const [state, setState] = useState(0)
+  const [description, setDescription] = useState('')
 
   return (
     <>
       <h1 className='text-center my-5'>Input Todo</h1>
       <form className='d-flex'>
-        <input type='text' className='form-control' placeholder='Add Todo' />
+        <input
+          type='text'
+          className='form-control'
+          placeholder='Add Todo'
+          value={description}
+          onChange={({ target }) => setDescription(target.value)}
+        />
         <button type='submit' className='btn btn-success '>
           Add
         </button>
