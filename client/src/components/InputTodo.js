ff7c787139f254bf7c9fb8539c6ec47498e9a1bd
Ikraam <IbnDaanis@Gmail.com>
2021-06-29 01:06:24 +0100

    handle submit add todo

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -3,10 +3,24 @@ import React, { useState } from 'react'
 export const InputTodo = () => {
   const [description, setDescription] = useState('')
 
+  const handleSubmit = async event => {
+    event.preventDefault()
+    try {
+      const body = { description }
+      await fetch('http://localhost:5000/todos', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body)
+      })
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
+
   return (
     <>
       <h1 className='text-center my-5'>Input Todo</h1>
-      <form className='d-flex'>
+      <form className='d-flex' onSubmit={handleSubmit}>
         <input
           type='text'
           className='form-control'
