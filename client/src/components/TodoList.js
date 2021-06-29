0ac066fb50050257d81b7ec6cea009aa47055163
Ikraam <IbnDaanis@Gmail.com>
2021-06-29 01:15:47 +0100

    getTodos

--- client/src/components/TodoList.js
+++ client/src/components/TodoList.js
@@ -3,7 +3,16 @@ import React, { useState, useEffect } from 'react'
 export const TodoList = () => {
   const [todos, setTodos] = useState([])
 
-  useEffect(() => {}, [])
+  const getTodos = async () => {
+    const response = await fetch('http://localhost:5000/todos')
+    const data = await response.json()
+
+    setTodos(data)
+  }
+
+  useEffect(() => {
+    getTodos()
+  }, [])
 
   return (
     <>
@@ -15,7 +24,15 @@ export const TodoList = () => {
             <th scope='col'>Delete</th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          {todos?.map(todo => (
+            <tr key={todo.todo_id}>
+              <th scope='row'>{todo.description}</th>
+              <td>Edit</td>
+              <td>Delete</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </>
   )
