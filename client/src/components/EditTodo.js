0ff3fa2e03b4917466d6e08690013333b6085b5a
Ikraam <IbnDaanis@Gmail.com>
2021-06-30 02:21:48 +0100

    edit todo

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -1,16 +1,34 @@
 import React, { useState } from 'react'
 
 export const EditTodo = ({ todo }) => {
+  const { todo_id, description } = todo
+  const [editedDescription, setEditedDescription] = useState(description)
+
+  const handleEditTodo = async id => {
+    const body = { description: editedDescription }
+    try {
+      await fetch(`http://localhost:5000/todos/${id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body)
+      })
+
+      window.location = '/'
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
+
   return (
     <>
       <button
         type='button'
         className='btn btn-warning'
         data-bs-toggle='modal'
-        data-bs-target={`#myModal-${todo.todo_id}`}>
+        data-bs-target={`#myModal-${todo_id}`}>
         Edit
       </button>
-      <div id={`myModal-${todo.todo_id}`} className='modal fade' role='dialog'>
+      <div id={`myModal-${todo_id}`} className='modal fade' role='dialog'>
         <div className='modal-dialog'>
           <div className='modal-content'>
             <div className='modal-header'>
@@ -20,10 +38,19 @@ export const EditTodo = ({ todo }) => {
               <h4 className='modal-title'>Edit Todo</h4>
             </div>
             <div className='modal-body'>
-              <input type='text' className='form-control' value={todo.description} />
+              <input
+                type='text'
+                className='form-control'
+                value={editedDescription}
+                onChange={({ target }) => setEditedDescription(target.value)}
+              />
             </div>
             <div className='modal-footer'>
-              <button type='button' className='btn btn-warning' data-bs-dismiss='modal'>
+              <button
+                type='button'
+                className='btn btn-warning'
+                data-bs-dismiss='modal'
+                onClick={() => handleEditTodo(todo_id)}>
                 Edit
               </button>
               <button type='button' className='btn btn-default' data-bs-dismiss='modal'>
